0aa3a83f310f7b3c53aaf1272c6122d310ba13d1
varshil patel <varshil20.work@gmail.com>
2026-05-22 09:36:04 +0530

    Time: 1 ms (99.48%) | Memory: 42.3 MB (92.53%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0){
+            return false;
+        }
+
+        int arr[] = new int[]{2,3,5};
+
+        for(int i=0 ; i<arr.length ; i++){
+            while(n%arr[i] == 0){
+                n = n/arr[i];
+            }
+        }
+
+        return n==1;
+
+    }
+}
