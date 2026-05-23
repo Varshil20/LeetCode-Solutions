cc8f11269d98196bca0b0fbfcc8447fa51cb7d2f
varshil patel <varshil20.work@gmail.com>
2026-05-23 10:01:02 +0530

    Time: 1 ms (91.60%) | Memory: 43 MB (89.17%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String s1 = "";
+
+        //HIGH SPACE COMPLEXITY
+
+        /*String s1 = "";
         for(int i=0 ; i<word1.length ; i++){
             s1 += word1[i];
         }
@@ -10,6 +13,17 @@ class Solution {
             s2 += word2[i];
         }
 
-        return s1.equals(s2);
+        return s1.equals(s2);*/
+
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        for(int i = 0; i<word1.length; i++){
+            str1.append(word1[i]);
+        }
+
+        for(int i = 0; i<word2.length; i++){
+            str2.append(word2[i]);
+        }
+        return str1.toString().equals(str2.toString());
     }
 }
