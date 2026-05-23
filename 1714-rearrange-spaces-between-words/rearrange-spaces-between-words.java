0206bd43a391dfffab6c9a1ca22b950c0bdcf101
varshil patel <varshil20.work@gmail.com>
2026-05-23 09:46:29 +0530

    Time: 1 ms (99.16%) | Memory: 42.2 MB (99.44%) - LeetSync

---
+++ 1714-rearrange-spaces-between-words/rearrange-spaces-between-words.java
@@ -0,0 +1,59 @@
+class Solution {
+    public String reorderSpaces(String text) {
+        int spaces = 0;
+        int words = 0;
+        int i = 0;
+
+        // Count spaces and words
+        while (i < text.length()) {
+
+            if (text.charAt(i) == ' ') {
+                spaces++;
+                i++;
+            }
+            else {
+
+                while (i < text.length() && text.charAt(i) != ' ') {
+                    i++;
+                }
+
+                words++;
+            }
+        }
+
+        int divide = 0;
+        int extra = 0;
+
+        if (words > 1) {
+            divide = spaces / (words - 1);
+            extra = spaces % (words - 1);
+        } else {
+            extra = spaces;
+        }
+
+        text = text.trim();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int j = 0; j < text.length(); j++) {
+
+            if (text.charAt(j) != ' ') {
+                sb.append(text.charAt(j));
+            }
+            else {
+
+                sb.append(" ".repeat(divide));
+
+                while (j < text.length() && text.charAt(j) == ' ') {
+                    j++;
+                }
+
+                j--;
+            }
+        }
+
+        sb.append(" ".repeat(extra));
+
+        return sb.toString();
+    }
+}
