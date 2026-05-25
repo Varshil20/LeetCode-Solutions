3520cdabf215ef9d958fa24aff4856cb65301b0a
varshil patel <varshil20.work@gmail.com>
2026-05-25 12:51:49 +0530

    Time: 3 ms (38.52%) | Memory: 43 MB (69.77%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,6 +1,9 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int freq[] = new int[26];
+        
+        //HIGH TIMECOMPLEXITY AND SPACE COMPLEXITY
+
+        /*int freq[] = new int[26];
 
         for(int i=0 ; i<s.length() ; i++){
             freq[s.charAt(i) - 97]++;
@@ -12,6 +15,20 @@ class Solution {
             return  t.charAt(i);
            }
         }
-        return ' ';
-    }
+        return ' ';*/
+
+        //ANOTHER METHOD
+
+        int sum=0;
+
+        for(int i=0 ; i<t.length() ; i++){
+            sum += t.charAt(i);
+        }
+
+        for(int i=0 ; i<s.length() ; i++){
+            sum-=s.charAt(i);
+        }
+
+        return (char)sum;
+    }   
 }
