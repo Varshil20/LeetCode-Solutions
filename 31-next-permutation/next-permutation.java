042bc5028c41fc30f34edb28d6e7c7fabd25bc16
varshil patel <varshil20.work@gmail.com>
2026-06-03 12:37:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (67.51%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int idx = -1;
+        int n = nums.length;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) {
+            swap(nums, 0, n - 1);
+        } else {
+
+            for (int i = n - 1; i > idx; i--) {
+                if (nums[i] > nums[idx]) {
+                    int temp = nums[i];
+                    nums[i] = nums[idx];
+                    nums[idx] = temp;
+                    break;
+                }
+            }
+
+            swap(nums, idx + 1, n - 1);
+        }
+    }
+
+    private void swap(int arr[], int s, int e) {
+        while (s < e) {
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+}
