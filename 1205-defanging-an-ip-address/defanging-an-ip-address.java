0ee66da2e5ab8f25befe650302494f9fb8d59cae
varshil patel <varshil20.work@gmail.com>
2026-06-09 13:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (12.54%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,6 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder sb = new StringBuilder();
+        /*StringBuilder sb = new StringBuilder();
 
         for(int i = 0 ; i < address.length() ; i++){
             if(address.charAt(i) != '.'){
@@ -11,6 +11,10 @@ class Solution {
             }
         }
 
-        return sb.toString();
+        return sb.toString();*/
+
+        //ANOTHER METHOD
+
+        return address.replace(".","[.]");
     }
 }
