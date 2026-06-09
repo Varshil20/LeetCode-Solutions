dae721f864ae882fd9727bd40d8f6bb664ab28b4
varshil patel <varshil20.work@gmail.com>
2026-06-09 12:33:34 +0530

    Time: 5 ms (99.78%) | Memory: 46.4 MB (76.03%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -1,6 +1,7 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        Set<String> ans = new HashSet<>();
+
+        /*Set<String> ans = new HashSet<>();
 
         for (int i = 0; i < emails.length; i++) {
             String splitarr[] = emails[i].split("@");
@@ -8,6 +9,38 @@ class Solution {
             ans.add(localname[0].replace(".", "") + "@" + splitarr[1]);
         }
 
+        return ans.size();*/
+
+        //ANOTHER MRTHOD
+        Set<String> ans = new HashSet<>();
+        for(int i = 0 ; i < emails.length ; i++){
+            String email = processEmail(emails[i]);
+
+            ans.add(email);
+        }
+
         return ans.size();
+  
     }
+
+     private String processEmail(String email){
+            int idx = email.indexOf('@');
+
+            StringBuilder sb = new StringBuilder();
+
+            //for loop to enter local name part before domain
+            for(int i = 0 ; i < idx ; i++){
+                char ch = email.charAt(i);
+                if(ch == '+'){
+                    break;
+                }
+                if(ch != '.'){
+                    sb.append(ch);
+                }
+            }
+
+            //append domain part
+            sb.append(email.substring(idx));
+            return sb.toString();
+        }
 }
