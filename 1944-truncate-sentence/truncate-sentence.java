20b4ec4880afaf036ffdd12ef1e31903ee8b1a0a
varshil patel <varshil20.work@gmail.com>
2026-06-09 13:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (86.55%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,6 +1,7 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        String splitarr[] = s.split(" ");
+
+        /*String splitarr[] = s.split(" ");
 
         StringBuilder sb = new StringBuilder();
 
@@ -8,6 +9,24 @@ class Solution {
             sb.append(splitarr[i] + " ");
         }
 
-        return sb.toString().trim(); 
+        return sb.toString().trim();*/
+
+        //ANOTHER METHOD
+
+        int count = 0;
+        int idx = -1;
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == ' '){
+                count++;
+            }
+
+            if(count == k){
+                idx = i;
+                break;
+            }
+        }
+
+        return (idx != -1) ? s.substring(0,idx) : s;
     }
 }
