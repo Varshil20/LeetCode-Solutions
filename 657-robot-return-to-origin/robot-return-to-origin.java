6b79ae8ca85f3e8f6c679568f5ccf16dd9e3067f
varshil patel <varshil20.work@gmail.com>
2026-06-10 16:43:31 +0530

    Time: 4 ms (98.62%) | Memory: 45.2 MB (97.38%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean judgeCircle(String moves) {
 
-       
-
-        int up = 0;
+        /*int up = 0;
         int right = 0;
 
         for(int i = 0 ; i < moves .length() ; i++){
@@ -25,9 +23,21 @@ class Solution {
             }
         }
 
-        return (up == 0 && right == 0);
+        return (up == 0 && right == 0);*/
 
-        //OR YOU CAN DO IT WITH FREQUENCY 
+        //OR YOU CAN DO IT WITH FREQUENCY
+
+        int freq[] = new int[26];
+
+        for(int i = 0 ; i < moves.length() ; i++){
+            freq[moves.charAt(i) - 'A']++;
+        }
+
+        if(freq[20]!=freq[3] || freq[11]!=freq[17]){ //this index is for U D L R
+            return false;
+        }
+
+        return true;
 
     }
 }
