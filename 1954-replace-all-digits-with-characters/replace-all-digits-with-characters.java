b3c0310b1f09cfb7ee63f40950f8ac3f10669aba
varshil patel <varshil20.work@gmail.com>
2026-06-12 22:24:58 +0530

    Time: 1 ms (43.55%) | Memory: 43 MB (54.43%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -2,14 +2,12 @@ class Solution {
     public String replaceDigits(String s) {
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0 ; i < s.length() ; i++){
-            if(i % 2 != 0){
-                char ans = shift(s.charAt(i - 1) , s.charAt(i));
-                sb.append(ans);
-            }
-            else{
-                sb.append(s.charAt(i));
-            }
+        for(int i = 1 ; i < s.length() ; i+=2){
+           sb.append(s.charAt(i-1));
+           sb.append(shift(s.charAt(i-1),s.charAt(i)));
+        }
+        if(s.length() % 2 == 1){
+            sb.append(s.charAt(s.length()-1));
         }
 
         return sb.toString();
