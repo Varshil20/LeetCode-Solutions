29734fc2e09f3bfcf611d2fb4f67432adfceb84c
varshil patel <varshil20.work@gmail.com>
2026-06-12 22:26:48 +0530

    Time: 1 ms (43.55%) | Memory: 43.1 MB (39.50%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -1,8 +1,8 @@
 class Solution {
     public String replaceDigits(String s) {
 
-        /*StringBuilder sb = new StringBuilder();
-
+        StringBuilder sb = new StringBuilder();
+        
         for(int i = 1 ; i < s.length() ; i+=2){
            sb.append(s.charAt(i-1));
            sb.append(shift(s.charAt(i-1),s.charAt(i)));
@@ -10,22 +10,25 @@ class Solution {
         if(s.length() % 2 == 1){
             sb.append(s.charAt(s.length()-1));
         }
-
+        
         return sb.toString();
-    }
+        }
+        
+        private char shift(char x , char n ){
+        return (char)(x + (n - '0'));
 
-    private char shift(char x , char n ){
-        return (char)(x + (n - '0')); */
+        //ANOTHER METHOD 
 
-         StringBuilder ans=new StringBuilder();
-        for(int i=0;i<s.length()-1;i+=2){
-            char ch=s.charAt(i);
+        /*StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            char ch = s.charAt(i);
             ans.append(ch);
-            int num=s.charAt(i+1)-'0';
-            char shifted=((char)(ch+num));
+            int num = s.charAt(i + 1) - '0';
+            char shifted = ((char) (ch + num));
             ans.append(shifted);
         }
-        if(s.length()%2==1) ans.append(s.charAt(s.length()-1));
-        return ans.toString();
+        if (s.length() % 2 == 1)
+            ans.append(s.charAt(s.length() - 1));
+        return ans.toString();*/
     }
 }
