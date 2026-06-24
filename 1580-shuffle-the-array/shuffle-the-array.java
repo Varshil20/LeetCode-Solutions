c714a97f3d4ae7c421ab4803254cc9ea64a1f6c0
varshil patel <varshil20.work@gmail.com>
2026-06-24 22:43:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (21.75%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int i = 0;
+        int j = n;
+        int ans[] = new int[nums.length];
+
+       for(int k = 0 ; k < nums.length-1 ; k+=2){
+        ans[k] = nums[i];
+        ans[k+1] = nums[j];
+        i++;
+        j++;
+       }
+
+        return ans;
+    }
+}
