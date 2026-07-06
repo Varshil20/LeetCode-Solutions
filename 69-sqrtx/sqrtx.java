95041bccac483c52999482eb48d296fe03eb4837
varshil patel <varshil20.work@gmail.com>
2026-07-06 15:11:54 +0530

    Time: 1 ms (97.66%) | Memory: 42.6 MB (61.03%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        /*int sqrt = (int)Math.sqrt(x);
+        
+        return sqrt; */
+
+        //ANOTHER METHOD
+
+        int l = 1;
+        int r = x;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (m > x / m) {
+                r = m - 1;
+            } else if (m < x / m) {
+                l = m + 1;
+            }
+
+            else
+                return m;
+        }
+
+        return r;
+    }
+}
