4cc1ff91e99e74a8b1a5d54ea731e75eb78694e5
varshil patel <varshil20.work@gmail.com>
2026-07-09 23:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (70.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findMid(0, nums.length - 1, nums, target, true);
+        /*int first = findMid(0, nums.length - 1, nums, target, true);
 
         if (first == -1) {
             return new int[]{-1, -1};
@@ -30,6 +30,68 @@ class Solution {
             } else {
                 start = mid + 1;
             }
+        }
+
+        return ans; */
+
+        //ANOTHER WAY INWHICH TAKE TWO LOOPS FOR LEFT SIDE AND RIGHT SIDE
+
+        int left = firstIdx(nums , target);
+
+        if(left == -1){
+            return new int[]{-1 , -1};
+        }
+
+        int right = lastIdx(nums , target);
+
+        return new int[]{left , right};
+    }
+
+    private int firstIdx(int []nums ,int target){
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                end = mid - 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int lastIdx( int [] nums , int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while(start <= end){
+           int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                start = mid + 1;
+            }
+
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+
+            else{
+                start = mid +1;
+            }
+            
         }
 
         return ans;
