168d6c50bd2e3b7a79de00ea6257dfb3495f53e9
varshil patel <varshil20.work@gmail.com>
2026-07-12 21:38:03 +0530

    Time: 27 ms (98.42%) | Memory: 77.4 MB (14.09%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int temp[] = arr.clone();
+        /*int temp[] = arr.clone();
 
         Arrays.sort(temp);
 
@@ -18,6 +18,23 @@ class Solution {
             arr[i] = map.get(arr[i]);
         }
 
-        return arr;
+        return arr;*/
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] nums=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            nums[i]=arr[i];
+        }
+        Arrays.sort(nums);
+        int ind=1;
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],ind++);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            nums[i]=map.get(arr[i]);
+        }
+        return nums;
     }
 }
