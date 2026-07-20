6ba05f55ff6f302ac55d24c077fd141c723cf911
varshil patel <varshil20.work@gmail.com>
2026-07-20 22:33:00 +0530

    Time: 6 ms (55.40%) | Memory: 47.5 MB (8.97%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int row = grid.length;
+        /*int row = grid.length;
         int col = grid[0].length;
         int n = row*col;
         k = k%n;
@@ -28,11 +28,43 @@ class Solution {
             ans.add(list);
         }
 
-        return ans;
+        return ans;*/
 
+        //ANOTHER METHOD
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        int n = row*col;
+        k = k%n;
+
+        swap(grid , 0 , n-1 , col);
+        swap(grid , 0 , k-1 , col);
+        swap(grid , k , n-1 , col);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < row; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < col; j++) {
+                list.add(grid[i][j]);
+            }
+            ans.add(list);
+        }
+
+        return ans;
     }
 
-    private void swap(int arr[] , int i , int j){
+    private void swap(int arr[][] , int i , int j , int col){
+                while(i < j){
+                    int temp = arr[i/col][i%col];
+                    arr[i/col][i%col] = arr[j/col][j%col];
+                    arr[j/col][j%col] = temp;
+                    i++;
+                    j--;
+                }
+    }
+
+    /* private void swap(int arr[] , int i , int j){
         while(i < j){
             int temp = arr[i];
             arr[i] = arr[j];
@@ -40,5 +72,5 @@ class Solution {
             i++;
             j--;
         }
-    }
+    }*/
 }
